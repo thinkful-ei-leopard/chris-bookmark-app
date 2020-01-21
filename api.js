16d9d1c298656786f3f801be01e30629b7453b20
Christopher Floyd <cfloyd252@gmail.com>
2020-01-21 10:08:33 -0700

    get and post bookmarks

---
+++ api.js
@@ -0,0 +1,24 @@
+
+//GET bookmarks
+function getBookmarks(){
+    return fetch(`https://thinkful-list-api.herokuapp.com/chris/bookmarks`);
+}
+
+//POST bookmarks
+function createBookmark(title, url, desc, rating){
+    let newBookmark = {
+        title,
+        url,
+        desc,
+        rating
+    };
+    newBookmark = JSON.stringify(newBookmark);
+    
+    return fetch(`https://thinkful-list-api.herokuapp.com/chris/bookmarks`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }, 
+        body: newBookmark})
+}
+
+//DELETE bookmark
+
